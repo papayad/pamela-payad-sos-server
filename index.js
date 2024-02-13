647f54fe38d3ef10df2e582028df27c249f4a6aa
pamela <payadpamela@gmail.com>
2024-02-13 18:05:30 -0500

    test basic server setup

---
+++ index.js
@@ -0,0 +1,10 @@
+require("dotenv").config();
+const express = require("express");
+const app = express();
+// const cors = require("cors");
+
+const PORT = process.env.PORT || 8081;
+
+app.listen(PORT, () => {
+  console.log(`⛰️ Server started on http://localhost:${PORT}`);
+});
