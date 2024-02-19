b91925ae553c997d8a65cd465f2226be1ab83ffd
pamela <payadpamela@gmail.com>
2024-02-19 13:20:31 -0500

    refactor routes with controllers

---
+++ controllers/user-controller.js
@@ -0,0 +1,12 @@
+const knex = require("knex")(require("../knexfile"));
+
+const getAllUsers = async (_req, res) => {
+  try {
+    const data = await knex("user");
+    res.status(200).json(data);
+  } catch (err) {
+    res.status(400).send(`Error retrieving Users: ${err}`);
+  }
+};
+
+module.exports = { getAllUsers };
